770be00311bd3e8bc5b9ff14da33ea9abb1cd399
Diwakarsharma2216 <diwakarsharma2216@gmail.com>
2025-05-29 14:57:11 +0530

    integrated-vapi

--- my-app/lib/actions/companion.actions.ts
+++ my-app/lib/actions/companion.actions.ts
@@ -38,3 +38,31 @@ export const getAllCompanions = async () => {
 
     return companions;
 }
+
+
+export const getCompanion = async (id: string) => {
+    const supabase = createSupabaseClient();
+
+    const { data, error } = await supabase
+        .from('companions')
+        .select()
+        .eq('id', id);
+
+    if(error) return console.log(error);
+
+    return data[0];
+}
+
+export const addToSessionHistory = async (companionId: string) => {
+    const { userId } = await auth();
+    const supabase = createSupabaseClient();
+    const { data, error } = await supabase.from('session_history')
+        .insert({
+            companion_id: companionId,
+            user_id: userId,
+        })
+
+    if(error) throw new Error(error.message);
+
+    return data;
+}
